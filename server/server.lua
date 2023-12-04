5e34bfffde52b4e24d4314bf2c7e73d6c5107014
Kian Shirvan <spoono17@gmail.com>
2023-12-04 08:18:46 -0700

    added check for player logging out

--- server/server.lua
+++ server/server.lua
@@ -1,13 +1,15 @@
 RegisterServerEvent("BGS_Archaeology_Free:GiveReward")
 AddEventHandler("BGS_Archaeology_Free:GiveReward", function(reward)
 	local _source = source
-	exports.vorp_inventory:addItem(_source, reward, 1)
+	if _source then
+		exports.vorp_inventory:addItem(_source, reward, 1)
+	end
 end)
 
 RegisterServerEvent("BGS_Archaeology_Free:GetShovelCount")
 AddEventHandler("BGS_Archaeology_Free:GetShovelCount", function()
 	local _source = source
-	if _source then
+	if _source and _source ~= nil then
 		local count = exports.vorp_inventory:getItemCount(_source, nil, Config.ShovelItem)
 		TriggerClientEvent("BGS_Archaeology_Free:GetShovelCountClient", _source, count)
 	end
